fa42fce27eaf1437c761ac799707c2ae416bf230
mmomrk <mark.mipt@yahoo.com>
2018-02-07 17:48:48 +0300

    added routines for rtc communications

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,5 @@
+#include <RTClib.h>
+
 #include <Wire.h>
 
 #include <SoftwareSerial.h>
@@ -6,7 +8,30 @@
 // * RX is digital pin 10 (connect to TX of other device)
 // * TX is digital pin 11 (connect to RX of other device)
 SoftwareSerial mySerial(10, 11, true); // RX, TX  //Do not refactor this line. DOn't know what this does and don't know how to hid it in the code
+RTC_DS1307 RTC;
 
+uint32_t getSeconds(void) {
+  DateTime now = RTC.now();
+  return now.unixtime();
+}
+
+void setupRTC(void) {
+  Wire.begin();
+  RTC.begin();
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+}
+
+void setupSerial(void) {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+  }
+  Serial.println("Serial is on, babe");
+}
 
 class Scales {
   private:
@@ -14,12 +39,7 @@ class Scales {
     long prevRecTime = 0;
     int result = 0;
     byte mesLeng = 0; //todo:rename
-  public:    void setupSerials() {
-      // Open serial communications and wait for port to open:
-      Serial.begin(57600);
-      while (!Serial) {
-      }
-      Serial.println("Serial is on, babe");
+  public:    void setupSWSerial() {
       // set the data rate for the SoftwareSerial port
       mySerial.begin(4800);
     }
@@ -61,14 +81,36 @@ class Scales {
 Scales *scales ;
 
 void setup() {
+  setupSerial();
   scales = new Scales();
-  scales->setupSerials();
+  scales->setupSWSerial();
+  setupRTC();
 
 }
 
-
+String getDateTimeS(void) {
+  DateTime now = RTC.now();
+  String dateTimeString = \
+                          String(now.year(), DEC) + \
+                          String('/') + \
+                          String(now.month(), DEC) + \
+                          String('/') + \
+                          String(now.day(), DEC) + \
+                          String(' ') + \
+                          String(now.hour(), DEC) + \
+                          String(':') + \
+                          String(now.minute(), DEC) + \
+                          String(':') + \
+                          String(now.second(), DEC) + \
+                          String('\n');
+  return dateTimeString;
+}
 void loop() { // run over and over
-  scales->listenScales();
+  //  scales->listenScales();
+
+  Serial.print(':');
+  Serial.println();
+  delay(1000);
 }
 
 
